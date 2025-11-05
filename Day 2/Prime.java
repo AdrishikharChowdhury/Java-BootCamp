81d1952b600eb615548a92ea782d080c1c01b883
AdrishikharChowdhury <93586036+AdrishikharChowdhury@users.noreply.github.com>
2025-11-05 11:31:25 +0000

    Updated the logic

--- Day 2/Prime.java
+++ Day 2/Prime.java
@@ -14,7 +14,7 @@ public class Prime {
             return;
         }
 
-        for (int i = 2; i < num; i++) {
+        for (int i = 2; i*i < num; i++) {
             if (num % i == 0) {
                 flag = true;
                 break;
